fdd4cd5ecff6b5b08b88ca93bb6880e506f8f2c0
Aaron Cordova <theqodesmith@gmail.com>
2018-05-10 07:40:21 -0400

    Oops.

--- untitled.js
+++
@@ -1,9 +0,0 @@
-const glob = require('glob-all')
-const path = require('path')
-
-var x = glob.sync([
-  path.resolve(__dirname, 'src/**/*.js'),
-  path.resolve(__dirname, 'src/index.ejs')
-])
-
-console.log(x)
